41723c5a0a7bfc8be5ac4578cdb5520f83930923
Ioana Manaila <ioana.ghiorghi@golfgenius.com>
2025-10-08 13:39:20 +0300

    Limit the number of scores in feed

--- spec/controllers/api/scores_controller_spec.rb
+++ spec/controllers/api/scores_controller_spec.rb
@@ -26,6 +26,30 @@ describe Api::ScoresController, type: :request do
       expect(scores[1]['total_score']).to eq 68
       expect(scores[2]['total_score']).to eq 79
     end
+
+    it 'should return at most 25 most recent scores' do
+      rng = Random.new
+
+      30.times do |i|
+        user = User.create!(name: "User#{i}", email: "user#{i}@example.com")
+        Score.create!(
+          user: user,
+          total_score: rng.rand(66..99),
+          played_at: Date.today - i.days
+        )
+      end
+
+      get api_feed_path
+
+      expect(response).to have_http_status(:ok)
+      response_hash = JSON.parse(response.body)
+      scores = response_hash['scores']
+
+      expect(scores.size).to eq 25
+
+      played_at_dates = scores.map { |s| Date.parse(s['played_at']) }
+      expect(played_at_dates).to eq played_at_dates.sort.reverse
+    end
   end
 
   describe 'POST create' do
